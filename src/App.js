3799d848004dd2ccf83d8b5989cbac4bf18f2457
rachelgrahamtohue@gmail.com <rachelgrahamtohue@gmail.com>
2022-03-31 21:46:37 +0100

    navigation bar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Data1 from './models/data.json';
 import ProductList from './components/ProductList'
 import Product from './components/Product';
+import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
+import Header from './components/Header';
+import About from './components/About';
+import Basket from './components/Basket';
 
 function App() {
 let basket = []
 let items = [...Data1]
-function removeFromBasket() {}
 function search() {}
 
 const addToBasket = (id) => {
@@ -15,27 +18,48 @@ basket=basket.concat(Data1.filter(item => item.trackId === id));
 console.log(basket)
   }
  
-  return (<ProductList product={items} addToBasket={addToBasket} />);
 
+  const removeFromBasket = (id) => {
+    basket=basket.filter(item => item.trackId !== id);
+    
+  }
+
+ 
+  return (
+ <Router>
+   <Header classname="App"/>
   
+   <Switch>
+    
 
-//   return (<ProductList product={Data1} removeFromBasket={removeFromBasket} />);
+      < Route path="/about">
+        <About/>
+      </Route>
+
+      < Route path="/basket">
+        <Basket/>
+      </Route>
+      <Route path="/">
+      
+
+      (<ProductList product={items} addToBasket={addToBasket} removeFromBasket={removeFromBasket}/>)
+
+    </Route>
+
+   </Switch>
+ </Router>)
   
-//   const removeFromBasket = (id) => {
-//     setBasket(basket.filter(item => item.trackId !== id));
-//     setItems([...items.map(item => {
-//       if (item.trackId === id) {
-//         item.inBasket = false;
-//         setTotal(total - item.trackPrice);
-//       }
-//       return item;
-//     }
-//     )]);
-//     setCount(count-1);
-//   }
-
-
-// }
 
 }
+ 
   export default App;
+
+  // setItems([...items.map(item => {
+    //   if (item.trackId === id) {
+    //     item.inBasket = false;
+    //     setTotal(total - item.trackPrice);
+    //   }
+    //   return item;
+    // }
+    // )]);
+    // setCount(count-1);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+
+
+function home(){
+    return (
+        <h1>About</h1>
+    )
+}
+
+
+export default home;
